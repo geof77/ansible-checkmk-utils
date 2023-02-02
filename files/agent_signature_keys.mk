f1bb04bc319a7dc1df4edb3338782d81ba36c7be
Geoffroy Stévenne <geoffroy.stevenne@ksz-bcss.fgov.be>
2023-02-02 19:47:08 +0100

    initial commit

---
+++ files/agent_signature_keys.mk
@@ -0,0 +1,22 @@
+# Written by Checkmk store
+# 
+# THIS IS JUST AN EXAMPLE FILE
+#
+agent_signature_keys.update({1: {'alias': 'Agent Deploy Key',
+     'certificate': '-----BEGIN CERTIFICATE-----\n'
+                    'MIIC1jCCAb4CAQEwDQYJKoZIhvcNAQEFBQAwMDEbMBkGA1UECgwSQ2hlY2tfTUsg\n'
+
+(...)
+
+                    'QJNPVQNS2D/5Gx6DT2XU0UEA3kSvc1dDYAdaYiYc5ANtOCIJDT/i9EHg75z9ATTj\n'
+                    '9WWKMhD89IpC5A==\n'
+                    '-----END CERTIFICATE-----\n',
+     'date': 1655967418.0,
+     'owner': 'cmkadmin',
+     'private_key': '-----BEGIN ENCRYPTED PRIVATE KEY-----\n'
+                    'MIIFLTBXBgkqhkiG9w0BBQ0wSjApBgkqhkiG9w0BBQwwHAQIMTc5eS7BlkgCAggA\n'
+
+(...)
+
+                    'L2WX5VDayVOBj61G9OEF9czRKcMT6BMSm43dolkMnHuM\n'
+                    '-----END ENCRYPTED PRIVATE KEY-----\n'}})
